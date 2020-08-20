6d3f2636e54122dad02e72b9007524d79052d0e3
bgunukul <bgunukul@LIN65001021.corp.capgemini.com>
2020-08-20 17:43:09 +0530

    Added system.out.println

--- myfirstseleniumdemo/src/main/java/com/capgemini/myfirstseleniumdemo/WindowHandles.java
+++ myfirstseleniumdemo/src/main/java/com/capgemini/myfirstseleniumdemo/WindowHandles.java
@@ -13,6 +13,7 @@ public class WindowHandles{
    }
 	
    public void windowHandle() throws Exception {
+	   System.out.println("This Web URl");
       System.setProperty("webdriver.chrome.driver", "C:\\Users\\bgunukul\\Downloads\\chromedriver.exe");
       WebDriver driver = new ChromeDriver();
       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
